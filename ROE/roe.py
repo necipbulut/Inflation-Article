d4e90863987ea1ee1253bc427961bdba4e6a5015
necipbulut <48358377+necipbulut@users.noreply.github.com>
2024-07-27 10:51:15 +0300

    Update roe.py

--- ROE/roe.py
+++ ROE/roe.py
@@ -1,12 +1,12 @@
 
 import pandas as pd
-
+#Import Data
 file_path = r'C:\Users\necip.bulut\OneDrive - Bahceşehir Üniversitesi\Masaüstü\inflation\roe\data.xlsx'
 
 capital_df = pd.read_excel(file_path, sheet_name='capital')
 profit_df = pd.read_excel(file_path, sheet_name='profit')
 
-# Year/Quarter sütunlarının isimlerini çek
+
 time_columns = capital_df.columns[4:48]
 
 sector_mapping = {
@@ -58,32 +58,31 @@ sector_mapping = {
     46: 'Food and Beverage'
 }
 
-# Sektörel ROE hesaplama fonksiyonu
+# Indivudual Firm Weights and ROE
 def calculate_sectoral_roe(capital_df, profit_df, time_columns):
     sectoral_roes = {}
 
     for col in time_columns:
-        # Sermaye ve kar verilerini al ve 0 değerlerini filtrele
+
         capital_data = capital_df[['firm_id', 'sector_id', col]].dropna()
         capital_data = capital_data[capital_data[col] != 0]
         profit_data = profit_df[['firm_id', col]].dropna()
         profit_data = profit_data[profit_data[col] != 0]
 
-        # Firmaların sektörel sermaye ağırlıklarını hesapla
+      
         sector_capital_sum = capital_data.groupby('sector_id')[col].sum().reset_index()
         capital_data = capital_data.merge(sector_capital_sum, on='sector_id', suffixes=('', '_total'))
         capital_data['weight'] = capital_data[col] / capital_data[col + '_total']
 
-        # Firmaların ROE'sini hesapla
+        
         firm_data = capital_data[['firm_id', 'sector_id', 'weight', col]].merge(profit_data, on='firm_id', suffixes=('_capital', '_profit'))
         
-        # Sütun adlarını kontrol edelim
+     
         capital_col = col + '_capital'
         profit_col = col + '_profit'
 
         firm_data['roe'] = firm_data[profit_col] / firm_data[capital_col]  # profit / capital
-        
-        # Sektörel ROE'yi hesapla (ağırlıklı ortalama)
+
         sector_roe = firm_data.groupby('sector_id').apply(lambda x: (x['roe'] * x['weight']).sum()).reset_index()
         sector_roe.columns = ['sector_id', col]
         
@@ -91,17 +90,14 @@ def calculate_sectoral_roe(capital_df, profit_df, time_columns):
 
     return sectoral_roes
 
-# Sektörel ROE'leri hesapla
+# Sectoral_ROE
 sectoral_roes = calculate_sectoral_roe(capital_df, profit_df, time_columns)
 
-
-
-# Sonuçları DataFrame'e dönüştür
 result_df = sectoral_roes[time_columns[0]].copy()
 for col in time_columns[1:]:
     result_df = result_df.merge(sectoral_roes[col], on='sector_id', how='outer')
 result_df['sector'] = result_df['sector_id'].map(sector_mapping)
 
-# Sonuçları kaydet
-result_df.to_excel(r'C:\Users\necip.bulut\OneDrive - Bahceşehir Üniversitesi\Masaüstü\inflation\roe\sectoral_roe.xlsx', index=False)
+# Export Results
+result_df.to_excel(r'C:\Users\necip.bulut\OneDrive - Bahceşehir Üniversitesi\Masaüstü\inflation\roe\Results_sectoral_roe.xlsx', index=False)
 
